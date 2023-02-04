b784459ad9b027b9edf360769306768b19235666
brovkin-vn <68548524+brovkin-vn@users.noreply.github.com>
2023-02-04 07:28:21 +0700

    Update config.php

--- data/config.php
+++ data/config.php
@@ -1,13 +1,8 @@
 <?php
 
-#$HostCA = "pnoc-server5"; 
-#$UserCA = "root"; 
-#$PassCA = "FXTQ9-6TDDF"; 
-#$DBCA = "casualty"; 
-
-$HostCA = "pnoc-server9.uku.evraz.com"; 
-$UserCA = "casualtyUser"; 
-$PassCA = "ASD123asd!"; 
-$DBCA = "casualty"; 
+$HostCA = "xxx.com"; 
+$UserCA = "User"; 
+$PassCA = "qwerwerqw"; 
+$DBCA = "asfdasf"; 
 
 ?>
